3009f68de0c94b9bfa389d8372c90f85fe15f918
Laedinator <62262634+Laedinator@users.noreply.github.com>
2023-11-28 14:09:43 +0100

    Add doublyLinkedLists with tests.

--- src/nl/hva/miw/ads/linkedlists/DoublyLinkedList.java
+++ src/nl/hva/miw/ads/linkedlists/DoublyLinkedList.java
@@ -10,8 +10,8 @@ public class DoublyLinkedList {
     private class Node {
         int value;
 
-        Node next=null;      // Link to next node
-        Node prev=null;      // Link to previous node
+        Node next = null;      // Link to next node
+        Node prev = null;      // Link to previous node
 
         public Node(int value) {
             this.value = value;
@@ -28,40 +28,77 @@ public class DoublyLinkedList {
         return size;
     }
 
-    /**
-     * Return the value in the list at position index.
-     *
-     * @param index
-     * @return
-     */
-    public int get( int index ) {
-        return -1;
+    public int get(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException("Too large of a number.");
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
     }
 
-    /**
-     * Add an element to the list at position index.
-     *
-     * @param index
-     * @param value
-     */
-    public void add( int index, int value ) {
+    public void add(int index, int value) {
         // Implement, create a new Node for this entry.
-
-        Node n = new Node( value );
-
-        // Implement the rest
+        Node n = new Node(value);
+        if (index < 0 || index > size) {
+            return;
+        }
+        size++;
+        if (index == 0) {
+            n.next = head;
+            head = n;
+            return;
+        }
+        Node current = head;
+        Node previous = null;
+        for (int count = 0; count < index - 1; count++) {
+            previous = current;
+            current = current.next;
+        }
+        if (current.next != null) {
+            n.prev = current;
+            current.next.prev = n;
+        }
+        n.next = current.next;
+        current.next = n;
 
     }
 
-    /**
-     * Remove an elmeent from the list at position index, if it exists.
-     *
-     * @param index
-     */
-    public void remove( int index ) {
+    public void remove(int index) {
         // Implement, remove the corresponding node from the linked list.
+        if (index < 0 || index > size) {
+            return;
+        }
+        Node current = head;
+        Node previous = null;
+
+        if (index == 0) {
+            head = current.next;
+            head.prev = null;
+            size--;
+        } else {
+            for (int i = 0; i < index; i++) {
+                if (i == index - 1) {
+                    previous = current;
+                }
+                current = current.next;
+            }
+            setNode(current, previous);
+        }
     }
 
+    private void setNode(Node current, Node before) {
+        if (current.next != null) {
+            current.next.prev = before;
+            before.next = current.next;
+        } else {
+            before.next = null;
+            current.prev = null;
+        }
+        size--;
+    }
 
     @Override
     public String toString() {
@@ -70,15 +107,12 @@ public class DoublyLinkedList {
         sb.append("DoublyLinkedList{size=").append(size).append("}");
 
         Node current = this.head;
-        while ( current != null ) {
+        while (current != null) {
             sb.append(" ");
-            sb.append( current.value );
+            sb.append(current.value);
             current = current.next;
         }
 
         return sb.toString();
     }
-
-
-
 }
